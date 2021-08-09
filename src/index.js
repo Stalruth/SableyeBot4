2e4ab7ed3d89ced93d22fcbb7b12335ed3e55edc
Stuart Rutherford <squirrel96@live.co.uk>
2021-08-09 20:39:33 +1000

    Housekeeping

--- src/index.js
+++ src/index.js
@@ -3,14 +3,15 @@
 const util = require('util');
 
 const Discord = require('discord.js');
+
+const CommandHandler = require('./CommandHandler.js');
+
 const Client = new Discord.Client({
   intents: [
     Discord.Intents.FLAGS.GUILDS,
     Discord.Intents.FLAGS.GUILD_MESSAGES
   ]
 });
-
-const CommandHandler = require('./CommandHandler.js');
 
 Client.once('ready', async () => {
   CommandHandler.onReady(Client);
