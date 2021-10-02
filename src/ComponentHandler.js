cdc7d7b867d69d424e925f64d4a77fc6dc06e6fe
Stuart Rutherford <squirrel96@live.co.uk>
2021-10-02 23:08:26 +1000

    Add debug logging to Component Handler

--- src/ComponentHandler.js
+++ src/ComponentHandler.js
@@ -9,6 +9,7 @@ function addComponent(pattern, process) {
 addComponent(/^filter_/, require('./components/filter.js'));
 
 function onComponentInteraction(req, res) {
+  console.log(req.body.id, req.body.data.custom_id);
   for(const command of commands) {
     if(req.body.data.custom_id.match(command.pattern)) {
       command.process(req, res);
