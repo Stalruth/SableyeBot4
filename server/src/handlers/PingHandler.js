8414c7dbf261e899e896c33601167fe073c801a2
Stuart Allan Rutherford <squirrel96@live.co.uk>
2022-01-30 19:42:56 +1100

    Remove ping handler; handled by middleware

--- server/src/handlers/PingHandler.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-async function onPing(req, res) {
-  res.json({type: 1});
-  return;
-}
-
-module.exports = onPing
-
--- server/src/sableye.js
+++ server/src/sableye.js
@@ -1,13 +1,9 @@
 'use strict';
 
-const { verifyKey } = require('discord-interactions');
-
 const { onApplicationCommand, onAutocomplete } = require('./handlers/AppCommandHandler.js');
 const onComponentInteraction = require('./handlers/MessageComponentHandler.js');
-const onPing = require('./handlers/PingHandler.js');
 
 const handlers = {
-  1: onPing,
   2: onApplicationCommand,
   3: onComponentInteraction,
   4: onAutocomplete
