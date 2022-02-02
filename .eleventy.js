5f7c2f6dfa3f86c63b2cb33d281cd30f69ee2280
Stuart Allan Rutherford <squirrel96@live.co.uk>
2022-02-02 23:24:50 +1100

    GDPR compliance for the site

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,4 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "static": "/" });
+  eleventyConfig.addPassthroughCopy({ "node_modules/mvp.css/mvp.css" : "/css/mvp.css" });
 };
