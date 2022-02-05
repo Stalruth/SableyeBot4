31e9f190fc632d2efcac1aff3cc762a69afe0a50
Stuart Allan Rutherford <squirrel96@live.co.uk>
2022-02-05 19:19:29 +1100

    Fix docs issues

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy({ "static": "/" });
-  eleventyConfig.addPassthroughCopy({ "node_modules/mvp.css/mvp.css" : "/css/mvp.css" });
+  eleventyConfig.addPassthroughCopy({ 'static': '/' });
+  eleventyConfig.addPassthroughCopy({ 'node_modules/mvp.css/mvp.css' : '/css/mvp.css' });
+  eleventyConfig.addGlobalData('layout', 'docs_layout.liquid');
+  eleventyConfig.addGlobalData('title', 'Sableye Bot');
+  eleventyConfig.addGlobalData('description', 'Competitive Pokémon Discord Bot');
 };
