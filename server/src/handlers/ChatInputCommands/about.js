34ade7d4f425a5d3da4cb2bb5fb8690ae909ca55
Stuart Allan Rutherford <squirrel96@live.co.uk>
2022-02-05 19:23:38 +1100

    Change URL in `/about`

--- server/src/handlers/ChatInputCommands/about.js
+++ server/src/handlers/ChatInputCommands/about.js
@@ -15,7 +15,7 @@ const process = function(interaction) {
       embeds: [buildEmbed({
         title: 'About SableyeBot',
         description: 'Competitive Pokemon Discord Bot',
-        url: 'https://github.com/StAlRuth/SableyeBot4',
+        url: 'https://sableye-bot.xyz/',
         author: {
           name: 'stalruth#3021',
           icon_url: 'https://cdn.discordapp.com/avatars/112038152390123520/56a380f68b4127e8bc49d8e08dd6bd6e.webp',
