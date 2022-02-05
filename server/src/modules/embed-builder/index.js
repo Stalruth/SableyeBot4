c12c17fe4c5f8c95c9d42a832d7cea7ade825cc4
Stuart Allan Rutherford <squirrel96@live.co.uk>
2022-02-05 19:23:51 +1100

    v4.0.0-rc9

--- server/src/modules/embed-builder/index.js
+++ server/src/modules/embed-builder/index.js
@@ -4,7 +4,7 @@ function buildEmbed(options) {
   const defaults = {
     color: 0x5F32AB,
     footer: {
-      text: `SableyeBot version 4.0.0-rc8`,
+      text: `SableyeBot version 4.0.0-rc9`,
       icon_url: 'https://cdn.discordapp.com/avatars/211522070620667905/6b037c17fc6671f0a5dc73803a4c3338.webp',
     },
   };
