fea3e8fe271d2146f70a0f3bccaa808d9c43ac34
Stuart Allan Rutherford <squirrel96@live.co.uk>
2022-03-03 19:35:45 +1100

    Use Sentry for logging

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@
 
 const { verifyKeyMiddleware } = require('discord-interactions');
 const express = require('express');
+const Sentry = require('@sentry/node');
+const Tracing = require('@sentry/tracing');
 
 const { sableye } = require('./src/sableye.js');
 
@@ -10,7 +12,22 @@ const PORT = process.env.PORT;
 
 const app = express();
 
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Tracing.Integrations.Express({ app }),
+  ],
+  tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+
+app.use(Sentry.Handlers.tracingHandler());
+
 app.post('/', verifyKeyMiddleware(PUBLIC_KEY), sableye);
+
+app.use(Sentry.Handlers.errorHandler());
 
 app.listen(PORT);
 
