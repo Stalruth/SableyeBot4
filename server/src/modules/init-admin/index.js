4294c88d29fecd1abcea32ed9a2c0fd4570d6e7d
Stuart Allan Rutherford <squirrel96@live.co.uk>
2022-03-04 11:50:50 +1100

    Remove old package

--- server/src/modules/init-admin/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const admin = require('firebase-admin');
-admin.initializeApp({
-  credential: admin.credential.cert(require('./key.json')),
-  databaseURL: process.env.DATABASE_URL,
-});
-
-module.exports = admin;
