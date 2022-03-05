cab7dfcb2c48dad501415f8972970cf828103336
Stuart Allan Rutherford <squirrel96@live.co.uk>
2022-03-05 20:00:55 +1100

    Add support server to `/about`

--- server/src/handlers/ChatInputCommands/about.js
+++ server/src/handlers/ChatInputCommands/about.js
@@ -27,18 +27,23 @@ const process = function(interaction) {
             inline: true,
           },
           {
-            name: 'Privacy Policy',
-            value: '[Click Here](https://sableye-bot.xyz/PRIVACY)',
-            inline: true,
-          },
-          {
-            name: 'Terms of Use',
-            value: '[Click Here](https://sableye-bot.xyz/TERMS)',
-            inline: true,
+            name: 'Support Server',
+            value: 'https://discord.gg/etUxhVfA7u',
+            inline: 'true',
           },
           {
             name: 'Language',
             value: 'JavaScript (Powered by [express.js](https://expressjs.com/))',
+          },
+          {
+            name: 'Privacy Policy',
+            value: 'https://sableye-bot.xyz/PRIVACY',
+            inline: true,
+          },
+          {
+            name: 'Terms of Use',
+            value: 'https://sableye-bot.xyz/TERMS',
+            inline: true,
           },
         ],
       })],
