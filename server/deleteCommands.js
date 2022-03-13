90bf6d4f200bfed7ddd9701e8b77d08381c31982
Stuart Allan Rutherford <squirrel96@live.co.uk>
2022-03-13 18:11:54 +1100

    Use OAuth for command updating

--- server/deleteCommands.js
+++ server/deleteCommands.js
@@ -2,23 +2,46 @@
 
 const fetch = require('node-fetch');
 
-const BOT_TOKEN = process.env.BOT_TOKEN;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const APP_ID = process.env.APP_ID;
 const GUILD_ID = process.env.GUILD_ID;
 
-const options = {
-  method: 'PUT',
-  headers: {
-    'Authorization': `Bot ${BOT_TOKEN}`,
-    'Content-Type': 'application/json',
-    'User-Agent': 'DiscordBot (github.com/Stalruth/Sableyebot4, 4.0.0-rc12)',
-  },
-  body: '[]',
-};
+function getOptions(token) {
+  return {
+    method: 'PUT',
+    headers: {
+      'Authorization': `Bearer ${token}`,
+      'Content-Type': 'application/json',
+      'User-Agent': `DiscordBot (github.com/Stalruth/Sableyebot4, v${process.env.npm_package_version})`,
+    },
+    body: '[]',
+  };
+}
+
+async function getToken(appId, clientSecret) {
+  const authBuffer = Buffer.from(`${appId}:${clientSecret}`, 'utf-8');
+  const data = new URLSearchParams();
+  data.append('grant_type', 'client_credentials');
+  data.append('scope', 'applications.commands.update');
+  const result = await fetch('https://discord.com/api/v10/oauth2/token', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Authorization': `Basic ${authBuffer.toString('base64')}`,
+    },
+    body: data,
+  });
+  if(!result.ok) {
+    throw new Error(await result.text());
+  } else {
+    return (await result.json()).access_token;
+  }
+}
 
 async function main() {
+  const token = await getToken(APP_ID, CLIENT_SECRET);
   const result = await fetch(`https://discord.com/api/v10/applications/${APP_ID}`
-    + (GUILD_ID ? `/guilds/${GUILD_ID}` : '') + `/commands`, options);
+    + (GUILD_ID ? `/guilds/${GUILD_ID}` : '') + `/commands`, getOptions(token));
   if(result.ok) {
     console.log('Commands deleted!');
   } else {
