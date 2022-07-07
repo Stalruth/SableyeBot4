02187c6401f26a84e884160779282f867937b76f
Stuart Allan Rutherford <squirrel96@live.co.uk>
2022-07-08 04:43:52 +1000

    Add generation to /learn output

--- server/src/ChatInputCommands/learn.js
+++ server/src/ChatInputCommands/learn.js
@@ -188,7 +188,7 @@ async function process(interaction, respond) {
     type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
     data: {
       embeds: [buildEmbed({
-        title: pokemon.name,
+        title: `[${args.gen}] ${pokemon.name}`,
         fields,
         color: colours.types[Data.toID(pokemon.types[0])],
       })],
