6055dd9a9d2464714da516ea6a432abfd060d49c
Stuart Allan Rutherford <squirrel96@live.co.uk>
2022-08-25 18:45:15 +1000

    Use `discord-verify` for validating packages

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 'use strict';
 
-import { verifyKeyMiddleware } from 'discord-interactions';
+import { webcrypto } from 'node:crypto';
+
+import { InteractionResponseType, InteractionType } from 'discord-interactions';
+import { verify } from 'discord-verify';
 import express from 'express';
 import Sentry from '@sentry/node';
 import Tracing from '@sentry/tracing';
@@ -11,6 +14,64 @@ const PUBLIC_KEY = process.env.PUBLIC_KEY;
 const PORT = process.env.PORT;
 
 const app = express();
+
+function verifyKeyMiddleware(clientPublicKey) {
+  if (!clientPublicKey) {
+    throw new Error('You must specify a Discord client public key');
+  }
+
+  return async function (req, res, next) {
+    const timestamp = (req.header('X-Signature-Timestamp') || '');
+    const signature = (req.header('X-Signature-Ed25519') || '');
+
+    async function onBodyComplete(rawBody) {
+      if (!await verify(rawBody, signature, timestamp, clientPublicKey, webcrypto.subtle)) {
+        res.statusCode = 401;
+        res.end('[discord-interactions] Invalid signature');
+        return;
+      }
+
+      const body = JSON.parse(rawBody.toString('utf-8')) || {};
+      if (body.type === InteractionType.PING) {
+        res.setHeader('Content-Type', 'application/json');
+        res.end(
+          JSON.stringify({
+            type: InteractionResponseType.PONG,
+          }),
+        );
+        return;
+      }
+
+      req.body = body;
+      next();
+    }
+
+    if (req.body) {
+      if (Buffer.isBuffer(req.body)) {
+        await onBodyComplete(req.body);
+      } else if (typeof req.body === 'string') {
+        await onBodyComplete(Buffer.from(req.body, 'utf-8'));
+      } else {
+        console.warn(
+          '[discord-interactions]: req.body was tampered with, probably by some other middleware. We recommend disabling middleware for interaction routes so that req.body is a raw buffer.',
+        );
+        // Attempt to reconstruct the raw buffer. This works but is risky
+        // because it depends on JSON.stringify matching the Discord backend's
+        // JSON serialization.
+        await onBodyComplete(Buffer.from(JSON.stringify(req.body), 'utf-8'));
+      }
+    } else {
+      const chunks = [];
+      req.on('data', (chunk) => {
+        chunks.push(chunk);
+      });
+      req.on('end', async () => {
+        const rawBody = Buffer.concat(chunks);
+        await onBodyComplete(rawBody);
+      });
+    }
+  };
+}
 
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
