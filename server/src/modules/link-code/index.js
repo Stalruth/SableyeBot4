33744731369b10fa7256ac02107e0614a34c4ca5
Stuart Allan Rutherford <squirrel96@live.co.uk>
2022-09-03 01:11:55 +1000

    Add space to Linking Codes

--- server/src/modules/link-code/index.js
+++ server/src/modules/link-code/index.js
@@ -3,7 +3,7 @@ function getLinkingCode(userIds) {
       .sort((a,b) => a < b ? -1 : a === b ? 0 : 1)
       .map(i => (i >> 22n) % 10000n)
       .map(i => i.toString().padStart(4, '0'))
-      .join('');
+      .join(' ');
 }
 
 export default getLinkingCode;
