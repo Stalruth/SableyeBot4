b7bbb177dd720e91404a9083060ea05e41b33f0b
Stuart Allan Rutherford <squirrel96@live.co.uk>
2022-09-26 18:27:21 +1000

    Formally make `/dt` an alias.

--- server/src/ChatInputCommands/dt.js
+++ server/src/ChatInputCommands/dt.js
@@ -1,34 +1,10 @@
-import { buildEmbed } from '#utils/embed-builder';
-
 import Data from './data.js';
 
-const definition = {
-  description: 'Deprecated; use `/data` instead.',
-  options: Data.definition.options,
-};
-
-async function process(interaction, respond) {
-  const responder = (response) => {
-    response.data.embeds.push(
-      buildEmbed({
-        title: "The `/dt` command has been renamed to `/data`.",
-        description: `Please use \`/data\` instead of \`/dt\`; the \`/dt\` name will be removed in a future update.\n\nAll parameters remain the same otherwise.`,
-        color: 0xffaa00,
-      })
-    );
-    respond(response);
-  };
-
-  await Data.command.process(interaction, responder);
-};
-
-const autocomplete = Data.command.autocomplete;
-
 export default {
-  definition,
-  command: {
-    process,
-    autocomplete,
-  }
+  definition: {
+    description: 'Alias for `/data`.',
+    options: Data.definition.options,
+  },
+  command: Data.command,
 };
 
--- server/src/MessageComponents/dt.js
+++ server/src/MessageComponents/dt.js
@@ -1,22 +1,4 @@
-import { buildEmbed } from '#utils/embed-builder';
-
 import Data from './data.js';
 
-async function process(interaction, respond) {
-  const innerRespond = (response) => {
-    const result = response;
-    result.data.embeds.push(
-      buildEmbed({
-        title: "The `/dt` command has been renamed to `/data`.",
-        description: `Please use \`/data\` instead of \`/dt\`; the \`/dt\` name will be removed in a future update.\n\nAll parameters remain the same otherwise.`,
-        color: 0xffaa00,
-      })
-    );
-    return respond(result);
-  }
-  return Data(interaction, innerRespond);
-
-};
-
-export default process;
+export default Data;
 
