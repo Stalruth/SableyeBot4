24623474bba4979d83e3932d425f638a4328f5e7
Stuart Allan Rutherford <stalruth@gmail.com>
2024-04-03 14:07:32 +1100

    Expire old command invocations

--- server/src/handlers/MessageComponentHandler.js
+++ server/src/handlers/MessageComponentHandler.js
@@ -1,3 +1,7 @@
+import { InteractionResponseFlags, InteractionResponseType } from 'discord-interactions';
+
+import { buildError } from '#utils/embed-builder';
+
 const processes = {};
 const modulePaths = {};
 
@@ -6,25 +10,48 @@ function addComponent(name, module) {
 };
 
 async function onComponentInteraction(req, res) {
-  try {
-    console.log(JSON.stringify({
-      interactionType: req.body.type,
-      guildId: req.body.guild_id,
-      id: req.body.id,
-      command: req.body.message.interaction_metadata?.name ?? req.body.message.interaction.name,
-      params: {
-        custom_id: req.body.data.custom_id,
-        values: req.body.data.values,
-      }
-    }));
-  } catch(e) {
-    console.error(JSON.stringify(req.body));
-    console.error(e);
-  }
-
   const respond = (response) => {
     res.json(response)
   }
+
+  // old structure so expire command
+  if(!req.body.message.interaction_metadata) {
+    respond({
+      type: InteractionResponseType.UPDATE_MESSAGE,
+      data: {
+        embeds: req.body.message.embeds,
+        components: []
+      },
+    });
+
+    await fetch(`https://discord.com/api/v10/webhooks/${req.body.application_id}/${req.body.token}`,
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          embeds: [
+            buildError('This command execution has expired, please run it again.')
+          ],
+          flags: InteractionResponseFlags.EPHEMERAL,
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+          'User-Agent': `DiscordBot (https://github.com/Stalruth/SableyeBot4, v${process.env.npm_package_version})`,
+        },
+      }
+    );
+    return;
+  }
+
+  console.log(JSON.stringify({
+    interactionType: req.body.type,
+    guildId: req.body.guild_id,
+    id: req.body.id,
+    command: req.body.message.interaction_metadata.name,
+    params: {
+      custom_id: req.body.data.custom_id,
+      values: req.body.data.values,
+    }
+  }));
 
   const fallbackHandler = (interaction, respond) => {
     respond({
@@ -37,13 +64,13 @@ async function onComponentInteraction(req, res) {
   }
 
   try {
-    await (processes[req.body.message.interaction_metadata?.name ?? req.body.message.interaction.name] ?? fallbackHandler)(req.body, respond);
+    await (processes[req.body.message.interaction_metadata?.name] ?? fallbackHandler)(req.body, respond);
   } catch (e) {
     console.error(JSON.stringify({
       interactionType: req.body.type,
       guildId: req.body.guild_id,
       id: req.body.id,
-      command: req.body.message.interaction_metadata?.name ?? req.body.message.interaction.name,
+      command: req.body.message.interaction_metadata?.name,
       params: {
         custom_id: req.body.data.custom_id,
         values: req.body.data.values,
