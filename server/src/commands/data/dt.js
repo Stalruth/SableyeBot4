a50bb5295c3402689ec22d1d82a3251d91ab786d
Stuart Allan Rutherford <stalruth@gmail.com>
2024-04-29 16:31:54 +1000

    Allow `/dt` to be used in DMs

--- server/src/commands/data/dt.js
+++ server/src/commands/data/dt.js
@@ -4,6 +4,8 @@ export default {
   definition: {
     description: 'Alias for `/data`.',
     options: Data.definition.options,
+    integration_types: Data.integration_types,
+    contexts: Data.contexts
   },
   command: Data.command,
   integration_types: Data.definition.integration_types,
