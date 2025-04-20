a04aeb072730e772f25f382441489283cef71816
Stuart Allan Rutherford <stalruth@gmail.com>
2025-04-20 15:02:19 +1000

    Fix avatar in footer

--- server/src/utils/embed-builder.js
+++ server/src/utils/embed-builder.js
@@ -3,7 +3,7 @@ function buildEmbed(options) {
     color: 0x5F32AB,
     footer: {
       text: `Sableye version ${process.env.npm_package_version}`,
-      icon_url: 'https://cdn.discordapp.com/avatars/211522070620667905/6b037c17fc6671f0a5dc73803a4c3338.webp',
+      icon_url: 'https://cdn.discordapp.com/avatars/1254384836685336616/45661f92f3f0a37a5551bc3c5c372549.webp',
     },
   };
   return Object.assign(defaults, options);
