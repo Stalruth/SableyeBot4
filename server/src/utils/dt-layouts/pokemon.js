22ab1bc3ac776a406ee7926f6f224c70d378feb3
Stuart Rutherford <squirrel96@live.co.uk>
2026-01-05 18:43:26 +1100

    Fix grammatical error.

--- server/src/utils/dt-layouts/pokemon.js
+++ server/src/utils/dt-layouts/pokemon.js
@@ -74,7 +74,7 @@ function pokemonInfo(pokemon, gen, page) {
     items.push(`**${statNames.join('/')} (BST)**: ${stats.join('/')} (${stats.reduce((acc, cur) => acc + cur, 0)})`);
     items.push(`**Weight**: ${pokemon['weightkg'].toFixed(1)}kg (**Low Kick**: ${lowKickPower(pokemon['weightkg'])} BP)`);
     if(pokemon['requiredItems']) {
-      items.push(`This Pokémon will always be holding${pokemon['requiredItems'].length > 1 ? ' one of' : ''} the ${pokemon['requiredItems'].join(', ')}`);
+      items.push(`This Pokémon will always be holding${pokemon['requiredItems'].length > 1 ? ' one of' : ''} the ${pokemon['requiredItems'].join(', ')}.`);
     }
     if(pokemon['requiredAbility']) {
       items.push(`This Pokémon will always have the Ability ${pokemon['requiredAbility']}.`);
